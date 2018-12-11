e4dab501dbd7054d21f441a96d9698c6b5f96fe5
yohn-dezmon <38733157+yohn-dezmon@users.noreply.github.com>
2018-12-11 11:38:38 -0600

    Update Scene.py

--- Scene.py
+++ Scene.py
@@ -1,1 +1,11 @@
-d
+class Scene_(object):
+  
+  def __init__(self, name, description,
+               examination, solved=False):
+    self.name = name
+    self.description = description
+    self.examination = examination
+    self.solved = False
+  
+  def enter(self):
+    print(f"blah blah blah: {self.name}")
