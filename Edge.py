3b83509f3fac4ec4623651c4b5d16fb11d31ce86
yohn-dezmon <38733157+yohn-dezmon@users.noreply.github.com>
2018-12-11 11:44:44 -0600

    Update Edge.py

--- Edge.py
+++ Edge.py
@@ -1,1 +1,14 @@
+import Scene
 
+class Edge_(Scene.Scene_):
+  """ Testing how to import attributes and methods 
+      from a Parent class, in this case Scene_
+      """
+  
+  def __init__(self, name, description, examination):
+    super(Edge_, self).__init__(name, description, examination)
+   
+  def enter(self):
+    super(Edge_, self).enter()
+    
+    
